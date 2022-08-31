2f63c90b5275dda6b8984141c8b5139c1a41a33d
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-08-31 00:58:42 -0400

    removing unnecessary comments

--- legged_gym/envs/base/legged_robot_nav.py
+++ legged_gym/envs/base/legged_robot_nav.py
@@ -63,7 +63,6 @@ class LeggedRobotNav(LeggedRobot):
         Args:
             actions (torch.Tensor): Tensor of shape (num_envs, num_actions_per_env)
         """
-        # print("in step")
         clip_actions = self.cfg.normalization.clip_actions
         self.actions = torch.clip(actions, -clip_actions, clip_actions).to(self.device)
         # step physics and render each frame
@@ -78,7 +77,6 @@ class LeggedRobotNav(LeggedRobot):
             if self.device == "cpu":
                 self.gym.fetch_results(self.sim, True)
             self.gym.refresh_dof_state_tensor(self.sim)
-        # print("calling post phys step")
         self.post_physics_step()
 
         # return clipped obs, clipped states (None), rewards, dones and infos
@@ -127,7 +125,6 @@ class LeggedRobotNav(LeggedRobot):
         self.compute_reward()
         self.compute_eval()
         env_ids = self.reset_buf.nonzero(as_tuple=False).flatten()
-        # print("in post phys calling reset idx")
         self.reset_idx(env_ids)
         self.compute_observations()  # in some cases a simulation step might be required to refresh some obs (for example body positions)
 
@@ -139,32 +136,15 @@ class LeggedRobotNav(LeggedRobot):
             self._draw_debug_vis()
 
     def save_im(self, im, path, height, width):
-        trans_im = im.detach().clone()
-        # print(trans_im)
+        trans_im = im.detach()
         trans_im = -1 / trans_im
         trans_im = trans_im / torch.max(trans_im)
-        # print("transformed im", trans_im)
-        # print("image size: ", trans_im.shape)
         save_image(
             trans_im.view((height, width, 1)).permute(2, 0, 1).float(),
             path,
         )
-        # if self.count == 500:
-        #     exit()
 
     def compute_observations(self):
-        """Computes observations"""
-        # print("lin vel shape: ", (self.base_lin_vel * self.obs_scales.lin_vel).shape)
-        # print("ang vel shape: ", (self.base_ang_vel * self.obs_scales.ang_vel).shape)
-        # print("gravity shape: ", (self.projected_gravity).shape)
-        # print("commands shape: ", (self.commands[:, :3] * self.commands_scale).shape)
-        # print(
-        #     "dof pos shape: ",
-        #     ((self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos).shape,
-        # )
-        # print("dof vel shape: ", (self.dof_vel * self.obs_scales.dof_vel).shape)
-        # print("actions shape: ", (self.actions).shape)
-        # exit()
         self.obs_buf = torch.cat(
             (
                 torch.tensor([[-1]]).cuda(),
@@ -192,9 +172,7 @@ class LeggedRobotNav(LeggedRobot):
                         gymapi.IMAGE_DEPTH,
                     )
                     im = gymtorch.wrap_tensor(im)
-                    # print(im)
                     image_buf[2 * i] = im
-                    # print("im shape: ", im.shape)
 
                     im2 = self.gym.get_camera_image_gpu_tensor(
                         self.sim,
@@ -203,9 +181,7 @@ class LeggedRobotNav(LeggedRobot):
                         gymapi.IMAGE_DEPTH,
                     )
                     im2 = gymtorch.wrap_tensor(im2)
-                    # print(im)
                     image_buf[2 * i + 1] = im2
-                    # print("im shape: ", im.shape)
 
                     if self.cfg.env.save_im:
                         path = f"images/dim/{i}_{self.count}_down.png"
