fed694b85f92b2d51fb99068a599ccae9d56446d
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-01 15:49:01 -0400

    setup now working on blade laptop

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 mamba create -n vl -y python=3.8 &&
-mamba install -n vl pytorch cudatoolkit=11.7 gdown tensorboard pytorch_memlab -c pytorch -c conda-forge -y &&
+conda activate vl &&
+mamba install pytorch torchvision cudatoolkit=11.6 -c pytorch -c conda-forge -y &&
+mamba install gdown tensorboard -c conda-forge -y &&
 gdown --fuzzy https://drive.google.com/file/d/1A4DwFb8Jj-PCFcKZE_SJA4b_oN5t9XSm/view?usp=sharing &&
 tar -xvf IsaacGym_Preview_3_Package.tar.gz &&
 rm IsaacGym_Preview_3_Package.tar.gz &&
-conda activate vl &&
-pip install -e isaacgym/python &&
+pip install -e isaacgym/python rsl_rl/. &&
 pip install -e legged_gym/. &&
-pip install -e rsl_rl/.
+pip install pytorch-memlab
