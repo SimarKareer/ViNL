4e298eb634da3bb9dac475c48419b27fd8984842
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-01 15:27:08 -0400

    using mamba instead

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-conda create -n vl -y python=3.8 &&
-conda install -n vl pytorch cudatoolkit=11.6 gdown -c pytorch -c conda-forge -y &&
+mamba create -n vl -y python=3.8 &&
+mamba install -n vl pytorch cudatoolkit=11.7 gdown tensorboard pytorch_memlab -c pytorch -c conda-forge -y &&
 gdown --fuzzy https://drive.google.com/file/d/1A4DwFb8Jj-PCFcKZE_SJA4b_oN5t9XSm/view?usp=sharing &&
 tar -xvf IsaacGym_Preview_3_Package.tar.gz &&
 rm IsaacGym_Preview_3_Package.tar.gz &&
+conda activate vl &&
 pip install -e isaacgym/python &&
-cd legged_gym/ &&
-pip install -e . &&
-pip install tensorboard pytorch_memlab
+pip install -e legged_gym/. &&
+pip install -e rsl_rl/.
