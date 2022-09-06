3b5120c2e7692915bc51bcc9f628bfb237d0cac5
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-06 11:06:17 -0400

    added change directory for habitat-lab installs

--- setup.sh
+++ setup.sh
@@ -8,14 +8,16 @@ rm IsaacGym_Preview_3_Package.tar.gz &&
 pip install -e isaacgym/python &&
 pip install -e rsl_rl/. &&
 pip install -e legged_gym/. &&
-pip install pytorch-memlab
+pip install pytorch-memlab &&
 
 # Habitat Lab installs
+cd habitat-lab &&
 pip install typing-extensions~=3.7.4 google-auth==1.6.3 simplejson braceexpand pybullet &&
 pip install -r requirements.txt &&
 python setup.py develop --all &&
 conda install -c conda-forge bullet -y &&
 pip install squaternion &&
+cd - &&
 
 # Need to install C++, and export LD_LIBRARY_PATH (~/miniconda3/envs/vl/lib/)
 sudo apt install build-essential # installs C++
