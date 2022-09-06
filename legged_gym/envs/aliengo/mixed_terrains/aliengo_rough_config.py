c0185d9d3330d4e325565717a2ffcc8bb3f2a641
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-06 14:03:10 -0400

    revert rough terrain to use default parameters for training

--- legged_gym/envs/aliengo/mixed_terrains/aliengo_rough_config.py
+++ legged_gym/envs/aliengo/mixed_terrains/aliengo_rough_config.py
@@ -53,19 +53,6 @@ class AliengoRoughCfg(LeggedRobotCfg):
         num_privileged_obs = None  # 187
         train_type = "priv"  # standard, priv, lbc
 
-    class commands:
-        curriculum = False
-        max_curriculum = 1.0
-        num_commands = 4  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
-        resampling_time = 10.0  # time before command are changed[s]
-        heading_command = True  # if true: compute ang vel command from heading error
-
-        class ranges:
-            lin_vel_x = [0.0, 0.5]  # min max [m/s]
-            lin_vel_y = [0.0, 0.0]  # min max [m/s]
-            ang_vel_yaw = [-np.deg2rad(60), np.deg2rad(60)]  # min max [rad/s]
-            heading = [-3.14, 3.14]
-
     class terrain(LeggedRobotCfg.terrain):
         mesh_type = "trimesh"
 
