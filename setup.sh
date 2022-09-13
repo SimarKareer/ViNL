19bcaa24efba56f82cf4c43e8205189e38c63806
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-09-13 16:55:48 -0400

    changing dirs when pip installing repo packages

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ mamba install gdown tensorboard -c conda-forge -y &&
 gdown --fuzzy https://drive.google.com/file/d/1A4DwFb8Jj-PCFcKZE_SJA4b_oN5t9XSm/view?usp=sharing &&
 tar -xvf IsaacGym_Preview_3_Package.tar.gz &&
 rm IsaacGym_Preview_3_Package.tar.gz &&
-pip install -e isaacgym/python &&
-pip install -e rsl_rl/. &&
-pip install -e legged_gym/. &&
+cd isaacgym/python && pip install -e . && cd - &&
+cd rsl_rl && pip install -e . && cd - &&
+cd legged_gym && pip install -e . && cd - &&
 pip install pytorch-memlab &&
 
 # Habitat Lab installs
-cd habitat-lab &&
+cd habitat-lab && git checkout aliengoisaac &&
 pip install typing-extensions~=3.7.4 google-auth==1.6.3 simplejson braceexpand pybullet &&
 pip install -r requirements.txt &&
 python setup.py develop --all &&
